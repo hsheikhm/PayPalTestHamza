8660ad3c5533c29779d6747f996b5ba39c140e80
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-31 00:24:04 +0000

    created test app using PayPal in Swift iOS

--- PayPalTestHamza/AppDelegate.swift
+++ PayPalTestHamza/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        // replace abc and xyz with client id
         PayPalMobile.initializeWithClientIdsForEnvironments([PayPalEnvironmentProduction: "AaAqHxwwgSS6LjhixZVQqFZNeCrjzaDgwIdSBvOoxptrcGAmDTvtrNrH4500aR7o9b42WUe-hTVq62hA", PayPalEnvironmentSandbox: "AUOe5U1jc9EbPxihVXMPlFVbsqcZwAan42In7Rue5QZZsAL3H3U9uSlQdBSKUyznjRUEBdAxTDGY_7KH"])
         
         return true
